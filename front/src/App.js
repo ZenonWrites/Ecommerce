12ecbf67584aa84b3db8a60a35250694e78059f0
ZenonWrites <145893524+ZenonWrites@users.noreply.github.com>
2025-08-18 20:03:48 +0530

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -660,7 +660,7 @@ function App() {
           </div>
 
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-300">
-            <p>&copy; 2025 Trécents. All rights reserved.</p>
+            <p>&copy; 2025 Trècénts. All rights reserved.</p>
           </div>
         </div>
       </footer>
