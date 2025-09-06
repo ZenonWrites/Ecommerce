d3d1f27956739e7f5c0f7a2c9f6177dfb158d7f2
ZenonWrites <saifsk3429@gmail.com>
2025-09-06 11:10:09 +0530

    Product Description fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', 'fallback-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
